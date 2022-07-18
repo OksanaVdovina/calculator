d8173e73b4593e48863fc9cc9960f372a3c43e4b
Oksana Vdovina <oksanavdovina@inbox.ru>
2022-07-18 09:26:54 +0700

    hw 2.4 исправлены недочеты

--- src/main/java/pro/sky/calculator/CalculatorService.java
+++ src/main/java/pro/sky/calculator/CalculatorService.java
@@ -8,24 +8,57 @@ public class CalculatorService {
         return "Добро пожаловать в калькулятор.";
     }
 
-    public String sumOfNumbers(int number1, int number2) {
-        int result = number1 + number2;
-        return number1 + " + " + number2 + " = " + result;
+    public String sumOfNumbers(Integer number1, Integer number2) {
+        if (number1 == null && number2 != null) {
+            return "Параметр num1 не задан";
+        } else if (number1 != null && number2 == null) {
+            return "Параметр num2 не задан";
+        } else if (number1 == null && number2 == null) {
+            return "Параметры num1 и num2 не заданы";
+        } else {
+            int result = number1 + number2;
+            return number1 + " + " + number2 + " = " + result;
+        }
     }
 
-    public String subtraction(int number1, int number2) {
-        int result = number1 - number2;
-        return number1 + " - " + number2 + " = " + result;
+    public String subtraction(Integer number1, Integer number2) {
+        if (number1 == null && number2 != null) {
+            return "Параметр num1 не задан";
+        } else if (number1 != null && number2 == null) {
+            return "Параметр num2 не задан";
+        } else if (number1 == null && number2 == null) {
+            return "Параметры num1 и num2 не заданы";
+        } else {
+            int result = number1 - number2;
+            return number1 + " - " + number2 + " = " + result;
+        }
     }
 
-    public String multiply(int number1, int number2) {
-        int result = number1 * number2;
-        return number1 + " * " + number2 + " = " + result;
+    public String multiply(Integer number1, Integer number2) {
+        if (number1 == null && number2 != null) {
+            return "Параметр num1 не задан";
+        } else if (number1 != null && number2 == null) {
+            return "Параметр num2 не задан";
+        } else if (number1 == null && number2 == null) {
+            return "Параметры num1 и num2 не заданы";
+        } else {
+            int result = number1 * number2;
+            return number1 + " * " + number2 + " = " + result;
+        }
     }
 
-    public String division(int number1, int number2) {
-        int result = number1 / number2;
-        return number1 + " / " + number2 + " = " + result;
+    public String division(Integer number1, Integer number2) {
+        if (number1 == null && number2 != null) {
+            return "Параметр num1 не задан";
+        } else if (number1 != null && number2 == null) {
+            return "Параметр num2 не задан";
+        } else if (number1 == null && number2 == null) {
+            return "Параметры num1 и num2 не заданы";
+        } else if (number2 == 0) {
+            return "На 0 делить нельзя";
+        } else {
+            int result = number1 / number2;
+            return number1 + " / " + number2 + " = " + result;
+        }
     }
-
 }
